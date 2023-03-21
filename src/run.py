cfd9195cfa9a3e43843606ebad0138e63cac7631
Justin D. Harris <juharris@users.noreply.github.com>
2023-03-20 20:29:31 -0400

    Enable looping

--- src/run.py
+++ src/run.py
@@ -2,6 +2,7 @@ import logging
 import os
 import re
 import sys
+import time
 from typing import Collection, List, Optional
 
 import yaml
@@ -13,52 +14,68 @@ from azure.devops.released.git import (Comment, GitClient, GitPullRequest,
                                        IdentityRef, IdentityRefWithVote)
 from msrest.authentication import BasicAuthentication
 
-config_path = sys.argv[1]
-print(f"Loading configuration from {config_path}")
-with open(config_path, 'r') as f:
-	config = yaml.safe_load(f)
 
-personal_access_token = config.get('PAT') or os.environ['CR_ADO_PAT']
-organization_url = config['organization_url']
-project = config['project']
-repository_id = config['repository_id']
-status = config.get('status', 'Active')
-rules = config['rules']
-top = config.get('top', 50)
-
-log_level = logging.getLevelName(config.get('log_level', 'INFO'))
-logging.basicConfig(level=log_level)
-
-current_user = config['current_user']
-user_id = config['user_id']
-is_dry_run = config.get('is_dry_run', False)
-
-credentials = BasicAuthentication('', personal_access_token)
-connection = Connection(base_url=organization_url, creds=credentials)
-core_client: CoreClient = connection.clients.get_core_client()
-git_client: GitClient = connection.clients.get_git_client()
 
 # See https://learn.microsoft.com/en-us/rest/api/azure/devops/git/pull-requests/get-pull-requests?view=azure-devops-rest-7.0&tabs=HTTP for help with what's possible.
 
-search = GitPullRequestSearchCriteria(repository_id=repository_id, status=status, )
-prs: Collection[GitPullRequest] = git_client.get_pull_requests(repository_id, search, project, top=top)
-
-attributes_with_patterns = ('description', 'title')
-for rule in rules:
-	for name in ('author',) + attributes_with_patterns:
-		if pat := rule.get(f'{name}_pattern'):
-			rule[f'{name}_regex'] = re.compile(pat, re.IGNORECASE)
-
 log_start = "*" * 100
+attributes_with_patterns = ('description', 'title')
 
-for pr in prs:
+def load_config(config_path: str) -> dict:
+	print(f"Loading configuration from {config_path}")
+	with open(config_path, 'r') as f:
+		result = yaml.safe_load(f)	
+
+	log_level = logging.getLevelName(result.get('log_level', 'INFO'))
+	logging.basicConfig(level=log_level)
+
+	rules = result['rules']
+	for rule in rules:
+		for name in ('author',) + attributes_with_patterns:
+			if pat := rule.get(f'{name}_pattern'):
+				rule[f'{name}_regex'] = re.compile(pat, re.IGNORECASE)
+	return result
+
+
+def review_prs(config: dict):
+	personal_access_token = config.get('PAT') or os.environ['CR_ADO_PAT']
+	credentials = BasicAuthentication('', personal_access_token)
+
+	organization_url = config['organization_url']
+	project = config['project']
+	repository_id = config['repository_id']
+	connection = Connection(base_url=organization_url, creds=credentials)
+	git_client: GitClient = connection.clients.get_git_client()
+
+	status = config.get('status', 'Active')
+	top = config.get('top', 50)
+	search = GitPullRequestSearchCriteria(repository_id=repository_id, status=status, )
+	prs: Collection[GitPullRequest] = git_client.get_pull_requests(repository_id, search, project, top=top)
+	for pr in prs:
+		try:
+			review_pr(config, git_client, pr)
+		except:
+			url = f"{organization_url}/{project}/_git/{repository_id}/pullrequest/{pr.pull_request_id}"
+			logging.exception(f"Error while reviewing pull request called \"{pr.title}\" at {url}")
+
+
+def review_pr(config: dict, git_client: GitClient, pr: GitPullRequest):
+	organization_url = config['organization_url']
+	project = config['project']
+	repository_id = config['repository_id']
+	rules = config['rules']
+
+	current_user = config['current_user']
+	user_id = config['user_id']
+	is_dry_run = config.get('is_dry_run', False)
+
 	author: IdentityRef = pr.created_by # type: ignore
 	reviewers: Collection[IdentityRefWithVote] = pr.reviewers # type: ignore
 	url = f"{organization_url}/{project}/_git/{repository_id}/pullrequest/{pr.pull_request_id}"
 	logging.debug(f"\n%s\n%s\nBy %s (%s)\n%s", log_start, pr.title, author.display_name, author.unique_name, url)
 
 	current_vote = None
-	reviewer = None
+	reviewer: Optional[IdentityRefWithVote] = None
 	for reviewer in reviewers:
 		if reviewer.unique_name == current_user:
 			current_vote = reviewer.vote
@@ -123,3 +140,22 @@ for pr in prs:
 				else:
 					logging.info("Would comment: \"%s\".", comment)
 				threads.append(thread)
+
+
+def main():
+	config_path = sys.argv[1]
+
+	while True:
+		config = load_config(config_path)
+		review_prs(config)
+
+		wait_after_review_s = config.get('wait_after_review_s')
+		if wait_after_review_s is not None:
+			logging.debug("Waiting %s seconds before the next review.", wait_after_review_s)
+			time.sleep(wait_after_review_s)
+		else:
+			break
+
+
+if __name__ == '__main__':
+	main()
