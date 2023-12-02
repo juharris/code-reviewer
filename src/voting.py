98b7fe6cdf89df5a9c942334f49b17470a0fb1ca
Justin D. Harris <juharris@users.noreply.github.com>
2023-12-01 21:28:33 -0500

    [voting] Allow approving if no vote is set

--- src/voting.py
+++ src/voting.py
@@ -1,6 +1,10 @@
 from typing import Optional
 
 
+APPROVE_VOTE = 10
+NO_VOTE = 0
+
+
 def map_vote(vote: Optional[int | str]) -> Optional[int]:
 	if vote is None or isinstance(vote, int):
 		return vote
@@ -12,11 +16,11 @@ def map_vote(vote: Optional[int | str]) -> Optional[int]:
 		case 'wait':
 			return -5
 		case 'none' | 'reset':
-			return 0
+			return NO_VOTE
 		case 'approve_with_suggestions':
 			return 5
 		case 'approve':
-			return 10
+			return APPROVE_VOTE
 	return None
 
 
@@ -33,3 +37,15 @@ def map_int_vote(vote: int) -> str | None:
 		case 10:
 			return 'APPROVE'
 	return None
+
+
+def is_vote_allowed(current_vote: int | None, new_vote: int | None) -> bool:
+	"""
+	Only vote if the new vote is more rejective (more negative) than the current vote,
+	the current vote is not set and the new vote is approve or approve with suggestions.
+	This is to avoid approving if someone has already voted.
+	"""
+	return new_vote is not None \
+		and (current_vote is None \
+			or new_vote < current_vote \
+			or (current_vote == NO_VOTE and new_vote > current_vote))
--- tests/test_voting.py
+++ tests/test_voting.py
@@ -1,16 +1,17 @@
-from voting import map_int_vote, map_vote
+from voting import APPROVE_VOTE, NO_VOTE, is_vote_allowed, map_int_vote, map_vote
 
 
 def test_map_vote():
 	assert map_vote(None) is None
-	assert map_vote(10) == 10
+	assert map_vote(10) == APPROVE_VOTE
+	assert map_vote(APPROVE_VOTE) == APPROVE_VOTE
 	assert map_vote(-10) == -10
 
 	assert map_vote('reject') == -10
 	assert map_vote('wait') == -5
-	assert map_vote('reset') == 0
+	assert map_vote('reset') == NO_VOTE
 	assert map_vote('approve_with_suggestions') == 5
-	assert map_vote('approve') == 10
+	assert map_vote('approve') == APPROVE_VOTE
 
 	assert map_vote('other') == None
 
@@ -19,8 +20,53 @@ def test_map_int_vote():
 	assert map_int_vote(-20) is None
 	assert map_int_vote(20) is None
 
-	assert map_int_vote(10) == 'APPROVE'
+	assert map_int_vote(APPROVE_VOTE) == 'APPROVE'
 	assert map_int_vote(5) == 'approve_with_suggestions'
-	assert map_int_vote(0) == 'reset'
+	assert map_int_vote(NO_VOTE) == 'reset'
 	assert map_int_vote(-5) == 'wait'
 	assert map_int_vote(-10) == 'REJECT'
+
+def test_allow_cote():
+	assert not is_vote_allowed(None, None)
+
+	assert is_vote_allowed(None, -10)
+	assert is_vote_allowed(None, -5)
+	assert is_vote_allowed(None, NO_VOTE)
+	assert is_vote_allowed(None, +5)
+	assert is_vote_allowed(None, APPROVE_VOTE)
+
+	assert not is_vote_allowed(-10, None)
+	assert not is_vote_allowed(-5, None)
+	assert not is_vote_allowed(NO_VOTE, None)
+	assert not is_vote_allowed(+5, None)
+	assert not is_vote_allowed(APPROVE_VOTE, None)
+
+	assert not is_vote_allowed(-10, -10)
+	assert not is_vote_allowed(-10, -5)
+	assert not is_vote_allowed(-10, NO_VOTE)
+	assert not is_vote_allowed(-10, 5)
+	assert not is_vote_allowed(-10, APPROVE_VOTE)
+
+	assert is_vote_allowed(-5, -10)
+	assert not is_vote_allowed(-5, -5)
+	assert not is_vote_allowed(-5, NO_VOTE)
+	assert not is_vote_allowed(-5, 5)
+	assert not is_vote_allowed(-5, APPROVE_VOTE)
+
+	assert is_vote_allowed(NO_VOTE, -10)
+	assert is_vote_allowed(NO_VOTE, -5)
+	assert not is_vote_allowed(NO_VOTE, NO_VOTE)
+	assert is_vote_allowed(NO_VOTE, 5)
+	assert is_vote_allowed(NO_VOTE, APPROVE_VOTE)
+
+	assert is_vote_allowed(5, -10)
+	assert is_vote_allowed(5, -5)
+	assert is_vote_allowed(5, NO_VOTE)
+	assert not is_vote_allowed(5, 5)
+	assert not is_vote_allowed(5, APPROVE_VOTE)
+
+	assert is_vote_allowed(APPROVE_VOTE, -10)
+	assert is_vote_allowed(APPROVE_VOTE, -5)
+	assert is_vote_allowed(APPROVE_VOTE, NO_VOTE)
+	assert is_vote_allowed(APPROVE_VOTE, 5)
+	assert not is_vote_allowed(APPROVE_VOTE, APPROVE_VOTE)
