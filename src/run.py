ec730ae299f9979fe1dd7ad7b87e6ec53caadf98
Marion Zepf <mazepf@microsoft.com>
2024-01-16 17:12:04 -0500

    Simplify auth for REST API calls

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@ import sys
 import time
 import urllib.parse
 from collections import Counter
-from typing import Collection, Optional
+from typing import Any, Collection, Optional
 
 import requests
 import yaml
@@ -44,23 +44,12 @@ POLICY_DISPLAY_NAME_JSONPATH = JSONPath('$.configuration.settings.displayName')
 ADO_REST_API_AUTH_SCOPE = '499b84ac-1321-427f-aa17-267ca6975798/.default'
 
 
-class BearerAuth(requests.auth.AuthBase):
-	token: str
-
-	def __init__(self, token):
-		self.token = token
-
-	def __call__(self, request):
-		request.headers["Authorization"] = f"Bearer {self.token}"
-		return request
-
-
 class Runner:
 	config: Config
 	config_hash: Optional[str] = None
 	git_client: GitClient
 	policy_client: PolicyClient
-	rest_api_auth: requests.auth.HTTPBasicAuth | BearerAuth
+	rest_api_kwargs: dict[str, Any]
 
 	def __init__(self, config_source: str) -> None:
 		self.config_source = config_source
@@ -179,7 +168,7 @@ class Runner:
 
 		if personal_access_token:
 			credentials = BasicAuthentication('', personal_access_token)
-			self.rest_api_auth = requests.auth.HTTPBasicAuth('', personal_access_token)
+			self.rest_api_kwargs = {'auth': requests.auth.HTTPBasicAuth('', personal_access_token)}
 
 		else:
 			managed_identity_client_id = os.environ.get('CR_MANAGED_IDENTITY_CLIENT_ID')
@@ -188,7 +177,7 @@ class Runner:
 				token = managed_identity_credential.get_token(ADO_REST_API_AUTH_SCOPE)
 				token_dict = {'access_token': token.token, 'expires_in': token.expires_on}
 				credentials = OAuthTokenAuthentication(managed_identity_client_id, token_dict)
-				self.rest_api_auth = BearerAuth(token.token)
+				self.rest_api_kwargs = {'headers': {"Authorization": f"Bearer {token.token}"}}
 
 			else:
 				raise ValueError("No personal access token and no managed identity client ID provided. Please set one of the environment variables CR_ADO_PAT or CR_MANAGED_IDENTITY_CLIENT_ID or set 'PAT' in the config file.")
@@ -542,14 +531,14 @@ class Runner:
 						# Use an undocumented API to get the diff.
 						# Found at https://stackoverflow.com/questions/41713616
 						diff_url =f'{organization_url}/{project}/_api/_versioncontrol/fileDiff?__v=5&diffParameters={{"originalPath":"{original_path}","originalVersion":"{diffs.base_commit}","modifiedPath":"{modified_path}","modifiedVersion":"{diffs.target_commit}","partialDiff":true,"includeCharDiffs":false}}&repositoryId={repository_id}'
-						diff_request = requests.get(diff_url, auth=self.rest_api_auth)
+						diff_request = requests.get(diff_url, **self.rest_api_kwargs)
 						diff_request.raise_for_status()
 						diff = diff_request.json()
 						result.append(FileDiff(change_type, modified_path, original_path=original_path, diff=diff))
 					elif change_type == 'add':
 						self.logger.debug("Getting new file \"%s\".", modified_path)
 						url = item['url']
-						request = requests.get(url, auth=self.rest_api_auth)
+						request = requests.get(url, **self.rest_api_kwargs)
 						request.raise_for_status()
 						contents = request.text
 						result.append(FileDiff(change_type, modified_path, contents=contents))
